c86eaf80bcc895f9539e108a9226ca9370ff6025
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2025-01-12 22:07:58 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -81,7 +81,7 @@ class Main extends Sprite
 	private function setupGame():Void
 	{
 		ClientPrefs.loadDefaultKeys();
-		addChild(new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen));
+		addChild(new FlxGame(gameWidth, gameHeight, #if (mobile && MODS_ALLOWED) !CopyState.checkExistingFiles() ? CopyState : #end initialState, framerate, framerate, skipSplash, startFullscreen));
 
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		#if !mobile
